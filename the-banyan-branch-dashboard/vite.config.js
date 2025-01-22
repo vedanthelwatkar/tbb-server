fad71584afc874f9382437b6551f7d6f00d8cca2
helwatkarVedant <vedant@unifynd.com>
2025-01-22 16:49:18 +0530

    removed base

--- the-banyan-branch-dashboard/vite.config.js
+++ the-banyan-branch-dashboard/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dashboard/",
   server: {
     port: 3000,
     host: true,
