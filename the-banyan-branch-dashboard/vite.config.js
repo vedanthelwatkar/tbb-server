564c5645921d80ac88c53dd4894ec6d10d1be2cf
helwatkarVedant <vedant@unifynd.com>
2025-01-22 16:33:29 +0530

    removed duplicate server

--- the-banyan-branch-dashboard/vite.config.js
+++ the-banyan-branch-dashboard/vite.config.js
@@ -5,14 +5,11 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-  },
-  plugins: [react()],
-  server: {
-    host: true,
     allowedHosts: [
       "thebanyanbranch.com",
       "www.thebanyanbranch.com",
       "52.66.111.49",
     ],
   },
+  plugins: [react()],
 });
