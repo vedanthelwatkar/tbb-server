960e77894a625434a7a38ff00f731e4a02d759dd
helwatkarVedant <vedant@unifynd.com>
2025-01-22 15:52:57 +0530

    changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    allowedHosts: [
+      "thebanyanbranch.com",
+      "www.thebanyanbranch.com",
+      "52.66.111.49",
+    ],
+  },
+});
