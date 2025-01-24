1f2db38a071b389287154ca429e7c83a90434e62
helwatkarVedant <vedant@unifynd.com>
2025-01-24 14:50:29 +0530

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: "thebanyanbranch.com",
+      protocol: "wss",
+    },
+  },
+  preview: {
     host: true,
-    allowedHosts: [
-      "thebanyanbranch.com",
-      "www.thebanyanbranch.com",
-      "52.66.111.49",
-    ],
+    port: 5173,
   },
 });
--- the-banyan-branch-dashboard/vite.config.js
+++ the-banyan-branch-dashboard/vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dashboard",
-  server: {
-    port: 3000,
-    host: true,
-    allowedHosts: [
-      "thebanyanbranch.com",
-      "www.thebanyanbranch.com",
-      "52.66.111.49",
-    ],
-  },
   plugins: [react()],
+  base: "/dashboard/",
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: "thebanyanbranch.com",
+      protocol: "wss",
+    },
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
 });
