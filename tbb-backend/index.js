07be4718decf3d837df6d7f72eb2c7950895a9fc
helwatkarVedant <vedant@unifynd.com>
2025-01-27 15:30:57 +0530

    auth removed in login and signup

--- tbb-backend/index.js
+++ tbb-backend/index.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(rateLimiter);
+app.use(rateLimiter);
 
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 app.use("/constants", constantsRoutes);
 app.use("/branding", brandingRoutes);
 app.use("/profile", profileRoutes);
