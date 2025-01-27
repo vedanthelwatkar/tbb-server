01fcbc47624ab1a2da9f63afe535624b236cd4fd
helwatkarVedant <vedant@unifynd.com>
2025-01-27 11:56:55 +0530

    db.js changed

--- tbb-backend/middlewares/rateLimiter.js
+++ tbb-backend/middlewares/rateLimiter.js
@@ -1,10 +1,9 @@
 import rateLimit from "express-rate-limit";
 
-const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 20,
-  skipSuccessfulRequests: true,
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+  max: 20, // Maximum of 20 requests in the 15-minute window
+  skipSuccessfulRequests: true, // Skip successful requests from counting against the limit
 });
-module.exports = {
-  authLimiter,
-};
+
+export { rateLimiter };
