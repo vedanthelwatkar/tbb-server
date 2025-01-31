fba44f6bc09aaec1eddeedec1c8517ea4cb299c3
helwatkarVedant <vedant@unifynd.com>
2025-01-31 11:55:13 +0530

    removed hmr from vite config

--- the-banyan-branch-dashboard/vite.config.js
+++ the-banyan-branch-dashboard/vite.config.js
@@ -3,16 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dashboard/",
   server: {
     host: "0.0.0.0",
     port: 3000,
     strictPort: true,
-    hmr: {
-      host: "thebanyanbranch.com",
-      protocol: "wss",
-    },
-    allowedHosts: ["thebanyanbranch.com", "www.thebanyanbranch.com"],
   },
   preview: {
     host: true,
