b08d7b56601c4c8b5b5852b77b11a94e7238677f
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-27 15:51:44 +0530

    dashboard issue fix

--- the-banyan-branch-dashboard/src/pages/Dashboard.jsx
+++ the-banyan-branch-dashboard/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
   let token = sessionStorage.getItem("token");
   const navigate = useNavigate();
+  
   useEffect(() => {
     if (!token) {
       navigate("/login");
